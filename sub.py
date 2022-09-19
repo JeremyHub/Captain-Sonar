ca3cf7353b28fbdd15e41a76563113f6dd3df9d6
JeremyHub <jeremyjhub@gmail.com>
2022-09-18 21:30:54 -0500

    added surfacing

--- sub.py
+++ sub.py
@@ -8,23 +8,17 @@ class Sub:
     breakdownMap: BreakdownMap
     powers: dict[Power, int]
     player: Player
+    remaining_surface_turns: int
 
 
-    def __init__(self,
-        player: Player,
-        *,
-        loc: tuple[int, int] = None,
-        path: list[tuple[int, int]] = None,
-        breakdownMap: BreakdownMap = None,
-        powers: dict[Power, int] = None,
-        damage: int = 0,
-    ):
+    def __init__(self, player: Player):
         self.player = player
-        self.loc = loc if loc else None
-        self.path = path if path else []
-        self.breakdownMap = breakdownMap if breakdownMap else BreakdownMap()
-        self.powers = powers if powers else {k:0 for k in Power}
-        self.damage = damage
+        self.loc = None
+        self.path = []
+        self.breakdownMap = BreakdownMap()
+        self.powers = {k:0 for k in Power}
+        self.damage = 0
+        self.remaining_surface_turns = 0
 
     
     def set_starting_loc(self, loc: tuple[int, int]):
@@ -33,11 +27,18 @@ class Sub:
 
     
     def move(self, direction):
-        self.path.append(self.loc)
-        self.loc = self._get_coord_in_direction(direction)
+        if direction is None and not self.remaining_surface_turns:
+            self._surface()
+        elif direction is None and self.remaining_surface_turns:
+            self.remaining_surface_turns -= 1
+        else:
+            self.path.append(self.loc)
+            self.loc = self._get_coord_in_direction(direction)
 
     
     def breakdown(self, dot, declared_direction):
+        if declared_direction is None: # surfacing
+            return
         for breakdown in self.breakdownMap.map[declared_direction]:
             if breakdown == dot:
                 assert not breakdown.marked, "trying to disable already marked breakdown"
@@ -55,6 +56,8 @@ class Sub:
     
     def get_active_powers(self):
         active = [None]
+        if self.remaining_surface_turns:
+            active
         for power, marks in self.powers.items():
             cost = POWER_COSTS[power]
             assert marks <= cost and marks >= 0, "{} on {} power".format(marks, power)
@@ -64,6 +67,8 @@ class Sub:
 
 
     def get_unbroken_breakdowns(self, direction):
+        if direction is None: # surfacing
+            return [None]
         options = []
         for breakdown in self.breakdownMap.map[direction]:
             if not breakdown.marked:
@@ -73,17 +78,20 @@ class Sub:
 
 
     def get_valid_directions(self, board):
-        valid_directions = []
+        valid_directions = [None]
+        if self.remaining_surface_turns:
+            return valid_directions
         for direction in Direction:
             row,col = self._get_coord_in_direction(direction)
             if 0 < row < len(board) and 0 < col < len(board[0]):
                 if board[row][col] == 0 and (row,col) not in self.path:
                     valid_directions.append(direction)
-        # TODO: add surfacing
         return valid_directions
 
     
     def get_unmarked_powers(self):
+        if self.remaining_surface_turns:
+            return [None]
         powers = [p for p in Power if p not in self.get_active_powers()]
         return powers if powers else [None]
 
@@ -129,3 +137,10 @@ class Sub:
                     if breakdown.channel == channel:
                         assert breakdown.marked, "channel was all marked, but one of the dots is not"
                         breakdown.marked = False
+
+    
+    def _surface(self):
+        assert self.remaining_surface_turns == 0, "trying to surface while already surfaced"
+        self.path = []
+        self.breakdownMap = BreakdownMap()
+        self.remaining_surface_turns = 3
