2aadce17989aa135536c30db1bd3df43717e2378
JeremyHub <jeremyjhub@gmail.com>
2022-10-10 12:55:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import players.random_player as randplayer
 if __name__ == "__main__":
     does_draw = False
     # does_draw = True
+    should_print = False
     g = Game(does_draw)
     num_games = 0
     p1 = randplayer.choose_action
@@ -12,19 +13,19 @@ if __name__ == "__main__":
     obs = None
     try:
         while True:
-            # print("---------------------------------------")
-            # print(f"player: {g.player.player}")
+            if should_print: print("---------------------------------------")
+            if should_print: print(f"player: {g.player.player}")
             options = g.legal_actions()
             if g.to_play() == 1:
                 action = p1(options, obs)
             elif g.to_play() == 2:
                 action = p2(options, obs)
-            # print("options: ", options)
-            # print("action: ", action)
+            if should_print: print("options: ", options)
+            if should_print: print("action: ", action)
             obs, reward, done = g.step(options[action])
-            # print("obs: ", obs)
-            # print("reward: ", reward)
-            # print("done: ", done)
+            if should_print: print("obs: ", obs)
+            if should_print: print("reward: ", reward)
+            if should_print: print("done: ", done)
             if done:
                 g = Game(does_draw)
                 num_games += 1
