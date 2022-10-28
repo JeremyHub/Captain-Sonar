b7c9f9ac4ecd2505acb26d0cba8f7bc506eeb0c9
JeremyHub <jeremyjhub@gmail.com>
2022-10-28 10:56:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -52,28 +52,37 @@ if __name__ == "__main__":
     all_num_turns = []
     p1_total_dmg = 1
     p2_total_dmg = 1
-    if does_draw:
-        pg = __import__("pygame")
-        for _ in range(num_games):
-            num_turns, p1_dmg, p2_dmg = run_one_game((does_draw, should_print, Random_Actor, Expert_Actor, 0))
-            p1_total_dmg += p1_dmg
-            p2_total_dmg += p2_dmg
-            all_num_turns.append(num_turns)
-            num_actual_games += 1
-    else:
+    p1_wins = 0
+    p2_wins = 0
+    
+    if not does_draw:
         pool = mp.Pool(processes=mp.cpu_count())
         map_result = pool.map_async(run_one_game, [(does_draw, should_print, Random_Actor, Expert_Actor, i) for i in range(num_games)])
         result_log = map_result.get()
         pool.close()
-        for num_turns, p1_dmg, p2_dmg in result_log:
-            p1_total_dmg += p1_dmg
-            p2_total_dmg += p2_dmg
-            all_num_turns.append(num_turns)
-            num_actual_games += 1
+    else:
+        pg = __import__("pygame")
+        result_log = []
+        for _ in range(num_games):
+            result_log.append(run_one_game((does_draw, should_print, Random_Actor, Expert_Actor, 0)))
+
+    for num_turns, p1_dmg, p2_dmg in result_log:
+        p1_total_dmg += p1_dmg
+        p2_total_dmg += p2_dmg
+        all_num_turns.append(num_turns)
+        num_actual_games += 1
+        if p2_dmg == 4:
+            p1_wins += 1
+        elif p1_dmg == 4:
+            p2_wins += 1
     print("----------------------final stats-----------------------------")
     print("total games finished: ", num_actual_games)
     print("p1 total dmg: ", p1_total_dmg)
     print("p2 total dmg: ", p2_total_dmg)
     print("ratio: ", p1_total_dmg/p2_total_dmg)
     print("avg turns: ", sum(all_num_turns)/len(all_num_turns))
+    print("p1 wins: ", p1_wins)
+    print("p2 wins: ", p2_wins)
+    print("p1 win ratio: ", p1_wins/num_actual_games)
+    print("p2 win ratio: ", p2_wins/num_actual_games)
     if does_draw: pg.quit()
