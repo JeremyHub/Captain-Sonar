1613e77814fbb622e435eb272de6e4ace2d70479
JeremyHub <jeremyjhub@gmail.com>
2022-11-09 15:42:13 -0600

    renamed made new files

---
+++ Captain_Sonar_Only_Move.py
@@ -0,0 +1,87 @@
+from game.game import CaptainSonar
+
+class CaptainSonar(AbstractGame):
+    """
+    Game wrapper.
+    """
+
+    def __init__(self, seed=None):
+        self.env = CaptainSonar(seed)
+
+    def step(self, action):
+        """
+        Apply action to the game.
+        Args:
+            action : action of the action_space to take.
+        Returns:
+            The new observation, the reward and a boolean if the game has ended.
+        """
+        observation, reward, done = self.env.step(action)
+        return observation, reward, done
+
+    def to_play(self):
+        """
+        Return the current player.
+        Returns:
+            The current player, it should be an element of the players list in the config.
+        """
+        return self.env.to_play()
+
+    def legal_actions(self):
+        """
+        Should return the legal actions at each turn, if it is not available, it can return
+        the whole action space. At each turn, the game have to be able to handle one of returned actions.
+        For complex game where calculating legal moves is too long, the idea is to define the legal actions
+        equal to the action space but to return a negative reward if the action is illegal.
+        Returns:
+            An array of integers, subset of the action space.
+        """
+        return self.env.legal_actions()
+
+    def reset(self):
+        """
+        Reset the game for a new game.
+        Returns:
+            Initial observation of the game.
+        """
+        return self.env.reset()
+
+    def render(self):
+        """
+        Display the game observation.
+        """
+        self.env.render()
+        input("Press enter to take a step ")
+
+    def human_to_action(self):
+        """
+        For multiplayer games, ask the user for a legal action
+        and return the corresponding action number.
+        Returns:
+            An integer from the action space.
+        """
+        choice = input(
+            f"Enter an action for player: {self.to_play()}: "
+        )
+        while choice not in [str(action) for action in self.legal_actions()]:
+            choice = input("Enter an action: ")
+        return int(choice)
+
+    def action_to_string(self, action_number):
+        """
+        Convert an action number to a string representing the action.
+        Args:
+            action_number: an integer from the action space.
+        Returns:
+            String representing the action.
+        """
+        return f"{action_number}. {self.env.REVERSE_ACTION_DICT[action_number]}"
+
+    def expert_agent(self):
+        """
+        Hard coded agent that MuZero faces to assess his progress in multiplayer games.
+        It doesn't influence training
+        Returns:
+            Action as an integer to take in the current game state
+        """
+        return self.env.random_action()
