4e3011080367fa8047d7203030d833b753e575c3
JeremyHub <jeremyjhub@gmail.com>
2022-11-14 13:51:58 -0600

    Update muzero_config.py

--- muzero_config.py
+++ muzero_config.py
@@ -15,7 +15,7 @@ class MuZeroConfig:
         self.observation_shape = (1, 1, 40)  # Dimensions of the game observation, must be 3D (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
         self.action_space = list(range(277))  # Fixed list of all possible actions. You should only edit the length
         self.players = list(range(2))  # List of players. You should only edit the length
-        self.stacked_observations = 1  # Number of previous observations and previous actions to add to the current observation
+        self.stacked_observations = 15  # Number of previous observations and previous actions to add to the current observation
 
         # Evaluate
         self.muzero_player = 0  # Turn Muzero begins to play (0: MuZero plays first, 1: MuZero plays second)
