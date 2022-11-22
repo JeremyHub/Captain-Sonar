7e6102aac8b65cc05f8aa7830f5825695660b6ca
JeremyHub <jeremyjhub@gmail.com>
2022-11-22 18:46:15 -0500

    path in config

--- muzero_config.py
+++ muzero_config.py
@@ -65,10 +65,10 @@ class MuZeroConfig:
 
 
         ### Training
-        self.results_path = None  # Path to store the model weights and TensorBoard logs
+        self.results_path = pathlib.Path(os.path.join("/content/drive/My Drive/results/captain_sonar_model", datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")))  # Path to store the model weights and TensorBoard logs  # Path to store the model weights and TensorBoard logs
         self.save_model = True  # Save the checkpoint in results_path as model.checkpoint
         self.training_steps = 100000  # Total number of training steps (ie weights update according to a batch)
-        self.batch_size = 512  # Number of parts of games to train on at each training step
+        self.batch_size = 128  # Number of parts of games to train on at each training step
         self.checkpoint_interval = 1  # Number of training steps before using the model for self-playing
         self.value_loss_weight = 1  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
         self.train_on_gpu = True if torch.cuda.is_available() else False  # Train on GPU if available
@@ -85,7 +85,7 @@ class MuZeroConfig:
 
 
         ### Replay Buffer
-        self.replay_buffer_size = 10000  # Number of self-play games to keep in the replay buffer
+        self.replay_buffer_size = 100  # Number of self-play games to keep in the replay buffer
         self.num_unroll_steps = 121  # Number of game moves to keep for every batch element
         self.td_steps = 121  # Number of steps in the future to take into account for calculating the target value
         self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
