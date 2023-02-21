0cb53b7f1c52ff0ba437aa94f5ffa415e64643a5
JeremyHub <jeremyjhub@gmail.com>
2023-02-21 12:27:36 -0600

    Update random_actor.py

--- actors/random_actor.py
+++ actors/random_actor.py
@@ -1,6 +1,8 @@
 from random import randint
 from typing import Any
 
+from ..game.breakdowns import BreakdownChannel
+
 from .actor import Actor
 
 class Random_Actor(Actor):
@@ -13,6 +15,16 @@ class Random_Actor(Actor):
         # if phase is mov or choose power, dont do nothing
         if len(actions) > 1 and obs[4] in [2]:
             action = randint(1,len(actions)-1)
+        elif obs[4] == 5 and len(actions) > 1:
+            good_breakdowns = []
+            for i, action in enumerate(actions):
+                if not self.reverse_action_dict[action].channel == BreakdownChannel.No_Channel:
+                    good_breakdowns.append(i)
+            if good_breakdowns:
+                action = randint(0,len(good_breakdowns)-1)
+                action = good_breakdowns[action]
+            else:
+                action = randint(0,len(actions)-1)
         else:
             action = randint(0,len(actions)-1)
         return actions[action]
